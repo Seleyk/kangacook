288998887bea8d9e01f03168cd6d1c94afd7ac3c
Steven Seley Jr <seleysteven2@gmail.com>
2024-08-23 19:48:32 -0500

    adding private routes (not finnished)

---
+++ client/src/Components/PrivateRoutes/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import api from "../../Api/api";
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../../Constants/constants";
+import { useState } from "react";
+
+
+
+const PrivateRoute = () => {
+
+
+}
+
+export default PrivateRoute
